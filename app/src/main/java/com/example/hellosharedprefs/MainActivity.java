c6977f1f4a0ca90ba60e67c92b4c3c5ba759ec23
danglvh1004 <lvhd.hdlv@gmail.com>
2021-02-07 09:15:03 +0700

    HelloSharedPrefs-Starter

---
+++ app/src/main/java/com/example/hellosharedprefs/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.hellosharedprefs;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int mCount = 0;
+    private int mColor;
+    private TextView mShowCountTextView;
+    private final String COUNT_KEY = "count";
+    private final String COLOR_KEY = "color";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mShowCountTextView = findViewById(R.id.count_textview);
+        mColor = ContextCompat.getColor(this,
+                R.color.default_background);
+
+    }
+
+    public void changeBackground(View view) {
+        int color = ((ColorDrawable) view.getBackground()).getColor();
+        mShowCountTextView.setBackgroundColor(color);
+        mColor = color;
+    }
+
+    public void countUp(View view) {
+        mCount++;
+        mShowCountTextView.setText(String.format("%s", mCount));
+    }
+
+    public void reset(View view) {
+        mCount = 0;
+        mShowCountTextView.setText(String.format("%s", mCount));
+
+        mColor = ContextCompat.getColor(this,
+                R.color.default_background);
+        mShowCountTextView.setBackgroundColor(mColor);
+    }
+}
